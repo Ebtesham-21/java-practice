dc34d525807f8010c51be74856793abe80c0ab58
unknown <ebteshamhossain98@gmail.com>
2022-02-15 14:36:56 +0600

    second one

--- Stack.java
+++ Stack.java
@@ -7,4 +7,27 @@ class Stack {
         stck = new int[size];
         tos = -1;
     }
+
+    // push an item onto the stck
+    void push(int item) {
+        if (tos == stck.length - 1) // use length member
+            System.out.println("Stack is full.");
+        else
+            stck[++tos] = item;
+    }
+
+    // pop an item from the stack
+    int pop() {
+        if (tos < 0) {
+            System.out.println("Stack underflow.");
+            return 0;
+        } else
+            return stck[tos--];
+    }
+}
+
+class TestStack2 {
+    public static void main() {
+
+    }
 }
